379b2f814d5a08c39b580995b32ff5d8b347fc0f
shravanibudhe27 <shravanibudhe27@gmail.com>
2026-04-12 18:43:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include 'db.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student CRUD</title>
+</head>
+<body>
+
+<h2>Add Student</h2>
+
+<form method="POST">
+    Name: <input type="text" name="name" required><br><br>
+    Email: <input type="email" name="email" required><br><br>
+    Mobile: <input type="text" name="mobile" required><br><br>
+    Department: <input type="text" name="department" required><br><br>
+    <input type="submit" name="submit" value="Add">
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $department = $_POST['department'];
+
+    $sql = "INSERT INTO student (name, email, mobile, department)
+            VALUES ('$name', '$email', '$mobile', '$department')";
+
+    mysqli_query($conn, $sql);
+}
+?>
+
+<h2>Student Records</h2>
+
+<table border="1">
+<tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Mobile</th>
+    <th>Department</th>
+    <th>Action</th>
+</tr>
+
+<?php
+$result = mysqli_query($conn, "SELECT * FROM student");
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<tr>
+        <td>{$row['id']}</td>
+        <td>{$row['name']}</td>
+        <td>{$row['email']}</td>
+        <td>{$row['mobile']}</td>
+        <td>{$row['department']}</td>
+        <td>
+            <a href='edit.php?id={$row['id']}'>Edit</a> |
+            <a href='delete.php?id={$row['id']}'>Delete</a>
+        </td>
+    </tr>";
+}
+?>
+
+</table>
+
+</body>
+</html>
